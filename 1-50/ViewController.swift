24cf6be683ccd0aa851e61b61fc5891f7cbdaac4
Nick Guerrero <nicolasjguerrero@gmail.com>
2017-09-10 21:44:17 -0400

    Push

--- 1-50/ViewController.swift
+++ 1-50/ViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 50
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String(indexPath.row + 1)
+        
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
